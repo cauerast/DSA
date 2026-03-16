55cff207997dc77c1fe12e9ecf59ac37fa2e70bb
cauerast <cauerast@gmal.com>
2026-03-16 00:32:37 -0300

    update exs

--- exs/exQueue/embarque-aeroporto.mjs
+++ exs/exQueue/embarque-aeroporto.mjs
@@ -29,5 +29,39 @@
     7-) Quem é o próximo passageiro a embarcar? Mostre o comando e o nome do passageiro.
 
     8-) O próximo passageiro foi chamado. Remova essa pessoa e imprima a fila atualizada.
+*/
 
-    */
+// 1 -> 
+import Queue from "../../lib/Queue.mjs";
+let airportQueue = new Queue();
+
+airportQueue.enqueue("passenger A");
+airportQueue.enqueue("passenger B");
+airportQueue.enqueue("passenger C");
+airportQueue.enqueue("passenger D");
+airportQueue.enqueue("passenger E");
+
+// 2 ->
+console.log("airport queue rn ->\n", airportQueue.print());
+
+// 3 ->
+console.log("\npassenger called ->", airportQueue.dequeue());
+
+// 4 ->
+console.log("\nairport queue rn ->\n", airportQueue.print());
+
+// 5 -> 
+airportQueue.enqueue("passenger F");
+airportQueue.enqueue("passenger G");
+airportQueue.enqueue("passenger H");
+
+// 6 ->
+console.log("\nairport queue rn ->\n", airportQueue.print());
+
+// 7 -> 
+console.log("\nnext passenger to be called ->", airportQueue.peek());
+
+// 8 -> 
+airportQueue.dequeue();
+console.log("\nairport queue rn ->\n", airportQueue.print());
+
