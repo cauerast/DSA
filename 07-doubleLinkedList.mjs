c60da0108964abc991972f83c796bd7f9b207415
cauerast <cauerast@gmail.com>
2026-04-08 22:25:26 -0300

    update dsa

--- 07-doubleLinkedList.mjs
+++ 07-doubleLinkedList.mjs
@@ -1,42 +1,24 @@
-import Linkedlist from "./lib/LinkendList.mjs";
+import DoublelinkedList from "./lib/Doublelinkedlist.mjs";
+let list = new DoublelinkedList();
 
-const list = new Linkedlist();
-
-console.log("list is empty? ", list.isEmpty ? "Yes":"No");
-
-list.insert(0, "Fusca");
-list.insert(0, "Corcel");
-console.log(list.print());
-
-list.insert(2, "Opala");
-list.insert(3, "Chevette");
-list.insert(4, "Maverick");
-console.log(list.print());
-
-list.insert(3, "Brasilia");
-console.log(list.print());
-
-list.inserHead("Belina");
-list.insertTail("Gol");
-console.log(list.print());
-
-let removed = list.remove(0);
-console.log({removed});
+console.log("list is empty ? ", list.isEmpty ? "yes" : "no");
 console.log(list.print())
 
-removed = list.remove(4);
-console.log({removed});
+list.insert(0, "Fiat 147");
+list.insert(1, "Opala");
+console.log("list is empty ? ", list.isEmpty ? "yes" : "no");
+console.log(list.print())
+
+list.insertTail("Chevette");
 console.log(list.print());
 
-let indexFusca = list.indexOf("Fusca");
-let indexOpala = list.indexOf("Opala");
-let indexGol = list.indexOf("Gol");
-let indexXXX = list.indexOf("XXX");
 
-console.log({indexFusca, indexGol, indexOpala, indexXXX});
+removed = list.remove(1);
+console.log({removed}, list.print());
 
-let idx0 = list.peek(0);
-let idx1 = list.peek(1);
-let idx4 = list.peek(4);
+let i0 = list.peek(0);
+let i1 = list.peek(1);
+console.log({i0, i1})
 
-console.log({idx0, idx1, idx4});
+let posChevette = list.indexOf("Chevette");
+console.log({posChevette});
