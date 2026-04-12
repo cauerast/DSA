795fbbe2100cecccd7edece3468b21a043ee25e5
cauerast <cauerast@gmail.com>
2026-04-12 10:07:01 -0300

    update dsa - create countOccurrences(n)

---
+++ exs/exLinkedList/ex3/testLinkedList.mjs
@@ -0,0 +1,15 @@
+import Linkedlist from "./LinkendList.mjs";
+
+let list = new Linkedlist();
+
+list.insert(0, 10);
+list.insert(1, 55);
+list.insert(2, 88);
+list.insert(3, 99);
+list.insert(4, 77);
+list.insert(5, 22);
+
+console.log(list.print());
+
+list.reverse();
+console.log(list.print());
